cb048a37d9da73fb3e228e81e714d8ef0441c943
Rafiul Islam <rafi.cse.ru@gmail.com>
2021-05-01 22:31:45 +0600

    Basic functionality done

---
+++ js/script.js
@@ -0,0 +1,91 @@
+mapboxgl.accessToken = "";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  //   console.log(position);
+  const { latitude, longitude } = position.coords;
+  setupPosition([longitude, latitude]);
+}
+
+function errorLocation() {
+  setupPosition([0, 0]);
+}
+
+function setupPosition(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 14,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, "top-left");
+  map.addControl(
+    new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    })
+  );
+  map.addControl(nav);
+
+  map.on("style.load", function () {
+    map.on("click", function (e) {
+      let coordinates = e.lngLat;
+      //   console.log(coordinates);
+      addToFavourites(coordinates);
+
+      //   new mapboxgl.Popup()
+      //     .setLngLat(coordinates)
+      //     .setHTML("you clicked here: <br/>" + coordinates)
+      //     .addTo(map);
+    });
+  });
+
+  function addToFavourites(coordinates) {
+    let ans = window.confirm("Do you want to save the location ?");
+    if (ans) {
+      let addressName = window.prompt("Name and an address");
+      let { lat, lng } = coordinates;
+
+      var fevAddress = document.getElementById("fevAddress");
+      var card = document.createElement("div");
+      card.className = "card";
+      var card_title = document.createElement("h3");
+      card_title.className = "card-title";
+      var latitude = document.createElement("span");
+      latitude.className = "latitude";
+      var longitude = document.createElement("span");
+      longitude.className = "longitude";
+      var nextLine = document.createElement("br");
+
+      card_title.innerText = addressName ? addressName : "Unknown";
+      latitude.innerText = "Latitude: " + lat.toFixed(2);
+      longitude.innerText = "Longitude: " + lng.toFixed(2);
+
+      fevAddress.appendChild(card);
+      card.appendChild(card_title);
+      card.appendChild(latitude);
+      card.appendChild(nextLine);
+      card.appendChild(longitude);
+    }
+
+    // console.log(obj);
+    // window.alert(
+    //   "AddressName: " +
+    //     addressName +
+    //     "\nLat: " +
+    //     lat.toFixed(2) +
+    //     "\nLng: " +
+    //     lng.toFixed(2)
+    // );
+  }
+}
